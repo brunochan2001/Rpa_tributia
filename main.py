03e05ce5021a9fbb1a129b6930f61fc8e8273f3b
brunochan2001 <brunoacy12@gmail.com>
2025-03-20 11:12:36 -0500

    I add improves

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 from dotenv import load_dotenv
@@ -56,22 +58,66 @@ driver = webdriver.Chrome(service=service, options=options)
 
 # Abrir la página de autenticación
 driver.get(LANDING_PAGE)
-time.sleep(10)
+
+try:
+    # Esperar hasta que el botón de inicio de sesión esté presente
+    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, "rutcntr")))
+    print("Página cargada correctamente, botón rutcntr.")
+except:
+    print("Error: La página tardó demasiado en cargar o el botón no se encontró.")
+
+try:
+    # Esperar hasta que el botón de inicio de sesión esté presente
+    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, "clave")))
+    print("Página cargada correctamente, botón clave.")
+except:
+    print("Error: La página tardó demasiado en cargar o el botón no se encontró.")
+
+try:
+    # Esperar hasta que el botón de inicio de sesión esté presente
+    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, "bt_ingresar")))
+    print("Página cargada correctamente, botón bt_ingresar.")
+except:
+    print("Error: La página tardó demasiado en cargar o el botón no se encontró.")
+
 driver.set_window_size(550, 692)
 
 # Ingresar credenciales
 driver.find_element(By.ID, "rutcntr").send_keys(USERNAME)
 driver.find_element(By.ID, "clave").send_keys(PASSWORD)
 driver.find_element(By.ID, "bt_ingresar").click()
-time.sleep(10)
+
+# Guardar la URL antes del login
+old_url = driver.current_url
+
+# Esperar hasta que la URL cambie
+WebDriverWait(driver, 20).until(EC.url_changes(old_url))
+print("Página cambiada correctamente. Nueva URL:", driver.current_url)
 
 # Archivos antes de la descarga
 before_files = set(os.listdir(download_dir)) 
 
+print("Archivos antes de la descarga:", before_files)
+
 # Navegar a la página de descarga
 DOWNLOAD_URL = f"{DOWNLOAD_PAGE}RUT_EMP={CLIENT_ID[:-1]}&DV_EMP={CLIENT_ID[-1]}&ORIGEN=RCP&RUT_RECP=&FOLIO=&FOLIOHASTA=&RZN_SOC=&FEC_DESDE=&FEC_HASTA=&TPO_DOC=&ESTADO=&ORDEN=&DOWNLOAD=XML"
 driver.get(DOWNLOAD_URL)
-time.sleep(20)
+
+download_file = None
+
+while download_file is None:
+    time.sleep(1)  # Espera 1 segundo para reducir el uso de CPU
+    after_files = set(os.listdir(download_dir))  
+    print("Archivos después de la descarga:", after_files)
+
+    # Identificar el nuevo archivo descargado
+    new_files = after_files - before_files  
+    valid_files = [f for f in new_files if not f.endswith((".crdownload", ".part", ".tmp"))]
+
+    if valid_files:  # Si hay un archivo válido nuevo, la descarga terminó
+        download_file = os.path.join(download_dir, valid_files[0])  
+
+print("Archivo descargado:", download_file)
 
 # Archivos después de la descarga
 after_files = set(os.listdir(download_dir)) 
