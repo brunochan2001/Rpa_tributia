87e46a4efaa3cf00081d599ea5338f097170b918
brunochan2001 <brunoacy12@gmail.com>
2025-03-22 11:02:18 -0500

    I upload logic of parquet and xml

--- main.py
+++ main.py
@@ -13,6 +13,10 @@ import boto3
 import gzip
 import time
 import os
+import xmltodict
+import json
+from more_itertools import flatten
+from flatten_dict import flatten
 
 current_path = os.getcwd()
 print("Current Path:", current_path)
@@ -60,36 +64,31 @@ driver = webdriver.Chrome(service=service, options=options)
 # Abrir la página de autenticación
 driver.get(LANDING_PAGE)
 
-try:
-    # Esperar hasta que el botón de inicio de sesión esté presente
-    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, "rutcntr")))
-    print("Página cargada correctamente, botón rutcntr.")
-except:
-    print("Error: La página tardó demasiado en cargar o el botón no se encontró.")
+# Ajustar tamaño de ventana
+driver.set_window_size(550, 692)  
 
-try:
-    # Esperar hasta que el botón de inicio de sesión esté presente
-    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, "clave")))
-    print("Página cargada correctamente, botón clave.")
-except:
-    print("Error: La página tardó demasiado en cargar o el botón no se encontró.")
+# Elementos a esperar en la página
+elementos = {
+    "rutcntr": "Página cargada correctamente, botón rutcntr.",
+    "clave": "Página cargada correctamente, botón clave.",
+    "bt_ingresar": "Página cargada correctamente, botón bt_ingresar."
+}
 
-try:
-    # Esperar hasta que el botón de inicio de sesión esté presente
-    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, "bt_ingresar")))
-    print("Página cargada correctamente, botón bt_ingresar.")
-except:
-    print("Error: La página tardó demasiado en cargar o el botón no se encontró.")
+# Esperar los elementos con un solo bucle
+for elem_id, mensaje in elementos.items():
+    try:
+        WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, elem_id)))
+        print(mensaje)
+    except:
+        print(f"Error: La página tardó demasiado en cargar o el botón {elem_id} no se encontró.")
 
-driver.set_window_size(550, 692)
+# Guardar la URL antes del login
+old_url = driver.current_url
 
 # Ingresar credenciales
 driver.find_element(By.ID, "rutcntr").send_keys(USERNAME)
 driver.find_element(By.ID, "clave").send_keys(PASSWORD)
 driver.find_element(By.ID, "bt_ingresar").click()
-
-# Guardar la URL antes del login
-old_url = driver.current_url
 
 # Esperar hasta que la URL cambie
 WebDriverWait(driver, 20).until(EC.url_changes(old_url))
@@ -98,43 +97,36 @@ print("Página cambiada correctamente. Nueva URL:", driver.current_url)
 # Archivos antes de la descarga
 before_files = set(os.listdir(download_dir)) 
 
-print("Archivos antes de la descarga:", before_files)
-
 # Navegar a la página de descarga
 DOWNLOAD_URL = f"{DOWNLOAD_PAGE}RUT_EMP={CLIENT_ID[:-1]}&DV_EMP={CLIENT_ID[-1]}&ORIGEN=RCP&RUT_RECP=&FOLIO=&FOLIOHASTA=&RZN_SOC=&FEC_DESDE=&FEC_HASTA=&TPO_DOC=&ESTADO=&ORDEN=&DOWNLOAD=XML"
 driver.get(DOWNLOAD_URL)
 
+# Esperar la descarga con límite de tiempo
 download_file = None
+timeout = 30  # Tiempo máximo de espera en segundos
+start_time = time.time()
 
-while download_file is None:
+while time.time() - start_time < timeout:
     time.sleep(1)  # Espera 1 segundo para reducir el uso de CPU
     after_files = set(os.listdir(download_dir))  
-    print("Archivos después de la descarga:", after_files)
 
     # Identificar el nuevo archivo descargado
     new_files = after_files - before_files  
     valid_files = [f for f in new_files if not f.endswith((".crdownload", ".part", ".tmp"))]
 
     if valid_files:  # Si hay un archivo válido nuevo, la descarga terminó
-        download_file = os.path.join(download_dir, valid_files[0])  
+        download_file = valid_files[0] 
+        break
 
-print("Archivo descargado:", download_file)
-
-# Archivos después de la descarga
-after_files = set(os.listdir(download_dir)) 
-print("after_files", after_files)
-
-# Identificar el nuevo archivo
-new_files = after_files - before_files  
-
-# Obtener el archivo descargado
-downloaded_file = new_files.pop()  
+# Si no se descargó ningún archivo, lanzar error
+if not download_file:
+    raise Exception("Error: No se detectó el archivo descargado dentro del tiempo de espera.")
 
 # Cerrar el navegador
 driver.quit()
 
 # Ruta del archivo descargado
-file_path = os.path.join(download_dir, downloaded_file)
+file_path = os.path.join(download_dir, download_file)
 
 # Comprimir el archivo en GZIP
 compressed_path = file_path + ".gz"
@@ -142,57 +134,85 @@ with open(file_path, "rb") as f_in, gzip.open(compressed_path, "wb") as f_out:
     shutil.copyfileobj(f_in, f_out)
 
 # Subir a S3 el archivo comprimido
-s3.upload_file(compressed_path, bucket_name, f"gzip/recibos/{downloaded_file}.gz")
+s3.upload_file(compressed_path, bucket_name, f"gzip/recibos/{download_file}.gz")
 print(f"Gzip file saved successfully!")
 
 # Define schema using PyArrow
 schema = pa.schema([
-    ('id', pa.int32()),
-    ('TipoDTE', pa.string()),
-    ('Folio', pa.string()),
-    ('FchEmis', pa.string()),
-    ('RznSoc', pa.string()),
-    ('MntTotal', pa.string()),
- 
+    ('DocumentoId', pa.string()),
+    ('TipoDTE', pa.int64()),
+    ('Folio', pa.int32()),
+    ('FechaEmision', pa.date32()),
+    ('FechaVencimiento', pa.date32()),
+    ('RazonSocial', pa.string()),
+    ('RutEmisor', pa.string()),
+    ('MontoNeto', pa.int32()),
+    ('MontoExe', pa.int32()),
+    ('TasaIVA', pa.int32()),
+    ('IVA', pa.int32()),
+    ('MontoTotal', pa.int32()),
 ])
 
-# parse the xml file
-tree = ET.parse(downloaded_file)
-root = tree.getroot()
+# Leer el contenido del archivo
+xml_content = open(download_file, "r").read()
 
-# Inicializar la estructura de datos
-valid_items = ["id","TipoDTE", "Folio", "FchEmis", "RznSoc", "MntTotal"]
-data = {key: [] for key in valid_items}
+# Convertir XML a diccionario
+data_dict = xmltodict.parse(xml_content)["SetDTE"]["DTE"]
 
-# Variable global para manejar el ID
-index = {"value": 1}
+# Extraer y normalizar los datos
+data = {
+    "DocumentoId": [],
+    "TipoDTE": [],
+    "Folio": [],
+    "FechaEmision": [],
+    "FechaVencimiento": [],
+    "RazonSocial": [],
+    "RutEmisor": [],
+    "MontoNeto": [],
+    "MontoExe": [],
+    "TasaIVA": [],
+    "IVA": [],
+    "MontoTotal": []
+}
 
-def parse_element(element, item):
-    if "id" not in item:  # Solo asignamos el id una vez por elemento raíz
-        item["id"] = index["value"]
-        index["value"] += 1 
-    
-    if(len(list(element)))==0:
-        item[element.tag] = element.text
-    else :
-        for child in list(element):
-            parse_element(child, item)
+# Extraer y normalizar los datos
+for child in data_dict:
+    documento = child.get("Documento", {})
+    detalles = documento.pop("Detalle", [])
 
-for child in root:
-    item = {}
-    parse_element(child,item)
-    # Agregar a la estructura de datos solo los elementos en valid_items
-    for key in valid_items:
-        data[key].append(item.get(key, None))  # Usar None si no existe
+    if not isinstance(detalles, list):
+        detalles = [detalles]  # Asegura que sea una lista para iterar
 
+    for detalle in detalles or [{}]:  # Si no hay detalles, agregar el documento solo
+        data["DocumentoId"].append(documento.get("@ID"))
+        data["TipoDTE"].append(documento.get("Encabezado", {}).get("IdDoc", {}).get("TipoDTE"))
+        data["Folio"].append(documento.get("Encabezado", {}).get("IdDoc", {}).get("Folio"))
+        data["FechaEmision"].append(documento.get("Encabezado", {}).get("IdDoc", {}).get("FchEmis"))
+        data["FechaVencimiento"].append(documento.get("Encabezado", {}).get("IdDoc", {}).get("FchVenc"))
+        data["RazonSocial"].append(documento.get("Encabezado", {}).get("Emisor", {}).get("RznSoc"))
+        data["RutEmisor"].append(documento.get("Encabezado", {}).get("Emisor", {}).get("RUTEmisor"))
+        data["MontoExe"].append(documento.get("Encabezado", {}).get("Totales", {}).get("MntExe"))
+        data["MontoNeto"].append(documento.get("Encabezado", {}).get("Totales", {}).get("MntNeto"))
+        data["TasaIVA"].append(documento.get("Encabezado", {}).get("Totales", {}).get("TasaIVA"))
+        data["IVA"].append(documento.get("Encabezado", {}).get("Totales", {}).get("IVA"))
+        data["MontoTotal"].append(documento.get("Encabezado", {}).get("Totales", {}).get("MntTotal"))
+
+# Convertir a DataFrame
 df = pd.DataFrame(data)
 
-# # Convert DataFrame to Arrow Table with the defined schema
+# Convertir columnas numéricas y manejar valores NaN de forma eficiente
+cols_int = ["TipoDTE", "Folio", "MontoNeto", "MontoExe", "TasaIVA", "IVA", "MontoTotal"]
+df[cols_int] = df[cols_int].apply(pd.to_numeric, errors="coerce").fillna(0).astype(int)
+
+# Convertir fechas
+df["FechaEmision"] = pd.to_datetime(df["FechaEmision"], format="%Y-%m-%d", errors="coerce")
+df["FechaVencimiento"] = pd.to_datetime(df["FechaVencimiento"], format="%Y-%m-%d", errors="coerce")
+
 table = pa.Table.from_pandas(df, schema=schema)
 
 # Save to a Parquet file
 parquet_path = file_path + ".parquet"
 pq.write_table(table, parquet_path)
 
-s3.upload_file(parquet_path, bucket_name, f"parquet/recibos/{downloaded_file}.parquet")
+s3.upload_file(parquet_path, bucket_name, f"parquet/recibos/{download_file}.parquet")
 print("Parquet file saved successfully!")
